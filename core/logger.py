c5b8da4ebdd81961fc9f843cc858e4d4eb47f199
Nobi004 <148976917+Nobi004@users.noreply.github.com>
2026-02-28 03:10:34 +0600

    Update logger.py

---
+++ core/logger.py
@@ -0,0 +1,17 @@
+import structlog
+from structlog.contextvars import merge_contextvars
+import logging
+
+structlog.configure(
+    processors=[
+        merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.JSONRenderer(),
+    ],
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+)
+
+logging.basicConfig(level=settings.log_level)
+logger = structlog.get_logger("ai_assessment")
